a66d939510f26c24d2ef5e222e4d36626d5002fb
Wamashudu Sengani <wamashudusengani2@gmail.com>
2022-10-29 23:32:39 +0200

    home page still needs a lot of work

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,45 +1,75 @@
 import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome,faUser,faLinkedin} from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faHome,faUser} from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { NavLink } from 'react-router-dom';
 
 
 const Sidebar = () => (
 
   <div class='main-container'>
         <div class='aside'>
-            <div class="logo">
-                <a href='#'>
-                    <span>W</span>
-                amashudu</a>
-            </div>
+         
             <div class='nav-toggler'>
                 
             </div>
-            <ul class="nav">
-                <li><a href='#' exact='true' class="active" to='/'><i class="fa-fa-home">
-                    <FontAwesomeIcon icon={faHome} color='#4d4d4e'/></i>Home</a></li>
-                <li><a href='#' exact='true' class="active" className='about-link' to='/about'><i class="fa-fa-user"> 
-                    <FontAwesomeIcon icon={faUser} color='#4d4d4e'/></i>About</a></li>
-                <li><a href='#' exact='true' class="active" className='contact-link' to='/contact'><i class="fa-fa-user"> 
-                    <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/></i>Contact</a></li>
+            <div class="nav">
+                <NavLink 
+                exact="true"
+                activeclassname="active"
+                to="/"
+                >
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                </NavLink>
+
+
+                <NavLink
+                 activeclassname="active"
+                 className="skills-link"
+                 to="/skills"
+                 >
+                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                 </NavLink>
+                
+                 <NavLink
+                    activeclassname="active"
+                    className="contact-link"
+                    to="/contact">
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
               
-            </ul>
+            </div>
 
-            <ul class='socials'>
-                <li><a target='_blank' rel='noreferrer' href='https://github.com/WamashuduSengani'><i class="fa-fa-user"> 
-                    <FontAwesomeIcon icon={faGithub} color='#4d4d4e'/></i>GitHub</a></li>
-                <li><a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/wamashudu-sengani-5911ba20a/'><i class="fa-fa-user"> 
-                    <FontAwesomeIcon icon={faLinkedinIn} color='#4d4d4e'/></i>Linkedin</a></li>
-            </ul>    
-
-        <div class='main-content'>
-        <section class='home section'>
-            Home
-
-        </section>
-
-        </div>
+            <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/slobodan-gaji%C4%87-006bb8b8/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/bobangajicsm"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        
+    
+      </ul>
 
 
     </div>
