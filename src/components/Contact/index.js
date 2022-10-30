187abd875b9bcd31d4159ed1385aec73458cc13f
Wamashudu Sengani <wamashudusengani2@gmail.com>
2022-10-30 17:07:33 +0200

    contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,17 @@
 import './index.scss'
 
-const Contact = () => ("Hello")
+const Contact = () => {
+    return (
+        <div className="contact">
+            <div class="container">
+                <h2>Contact me</h2>
+            </div>
+            <div>
+                <h5 class="contact-tile"></h5>
+            </div>
+
+        </div>
+    )
+}
 
 export default Contact
