4ced7c3320e93c3c2eb30724931a848e3b7bd8f5
Wamashudu Sengani <wamashudusengani2@gmail.com>
2022-10-30 22:04:36 +0200

    home page refactored

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,15 @@
 import './index.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faCss3,faGitAlt,faHtml5,faJava, faPython,faReact}
-from '@fortawesome/free-brands-svg-icons'
+import { NavLink } from 'react-router-dom'
+import {
 
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJava,
+  faPython,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class TxtType {
   constructor(el, toRotate, period) {
@@ -48,78 +55,85 @@ class TxtType {
   }
 }
 
-
 window.onload = function() {
   
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
-    }
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
 
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
 };  
 
+
 const Home = () => {
-    return (
-        
-        <div class='main-content'>
-        <div class="container">
-            <div class="row">
-                <div class="home-info padd-15" >
-                    <h3 class="hello">Hello, my name is <span class="name">Wamashudu Sengani</span></h3>
-                    
-                    <h3 class="my-profession">
-                        <a href="" class="typewrite" data-period="2000" data-type='[ "I am a Software Developer.", "I Love to Develop.", "I Love Design."]'
-                         ><span class="wrap"></span></a>
-                    </h3>
 
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+        <h1 class="hello">Hello, my name is <span class="name">Wamashudu Sengani</span></h1>
 
-                    <p class="my-self"><br/>I am a goal driven individual passionate about technology.<br/>
-                    I want to bring innovations to the world and use technology to enhance life.<br/>
-                    I'm a team player with excellent communication skills.<br/>
-                    I'm able to work effectively as an individual and within a team.
-                    <br/>I'm a critical thinker, I possess good problem solving skills <br/>
-                     and I always strive for continued excellence.</p>
+        <h3 class="my-profession">
+        <a href="" class="typewrite" data-period="2000" data-type='[ "I am a Software Developer.", "I Love to Develop.", "I Love Design."]'
+         ><span class="wrap"></span></a>
+         </h3>
 
-                     <h4 class='reason'>Why do I want to be a SovTech Developer?</h4>
-                     <p class="why">I just want to write code honestly</p>
-                   
-                    <a href='#contact' class='btn hire-me'>More about me</a>
-                </div>
-            </div>
-               <div className="stage-cube-cont">
-              <div className="cubespinner">
-                <div className="face1">
-                  <FontAwesomeIcon icon={faJava} color="#DD0031" />
-                </div>
-                <div className="face2">
-                  <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-                </div>
-                <div className="face3">
-                  <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-                </div>
-                <div className="face4">
-                  <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-                </div>
-                <div className="face5">
-                  <FontAwesomeIcon icon={faPython} color="#EFD81D" />
-                </div>
-                <div className="face6">
-                  <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
-                </div>
-              </div>
-            </div>
+         <h5>
+          <p>I am a goal driven individual passionate about technology.
+             I want to bring innovations to the world and use technology to enhance life.
+             I'm a team player with excellent communication skills.
+             I'm able to work effectively as an individual and within a team. I'm a critical thinker, I possess good problem solving skills 
+             and I always strive for continued excellence.
+           </p>
+          </h5>
+
+          <h4 class='reason'>Why do I want to be a SovTech Developer?</h4>
+          <p>I want to become part of the team that is changing the 
+             way business across the world build software.I related a lot with the SovTech #TECH spex
+             because I want to innovate and change the world using technology. I love problem solving and learning while
+             doing so. I see SovTech as a place where I can grow as a developer since I'm transitioning from being a student to getting into the
+             industry professionally, there's plenty of opportunities for continuous learning for me as I'm eager to learn.
+          </p>     
+
+          <NavLink activeclassname="active" className="flat-button" to="/contact">Contact me
+           </NavLink>
+       
         </div>
-  </div>
-    );
+      </div>
+
+      <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faJava} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faPython} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+
+    </>
+  )
 }
 
-
-export default Home;
+export default Home
